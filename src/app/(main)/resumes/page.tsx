1ecc75da78f31fd4fac00c2aba01703db830fa7c
Your Name <ekka.beaula@gmail.com>
2025-04-30 23:40:28 +0200

    set up prettier

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { PlusSquare } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
 
 export const metadata = {
-    title: "Resumes",
-    description: "Create a perfect resume in minutes",
-    };
+  title: 'Resumes',
+  description: 'Create a perfect resume in minutes',
+};
 export default function page() {
   return (
     <main>
-        This is resumes page
-      
+      This is resumes page
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href="/editor">
+          <PlusSquare className="size-5" /> New Resume
+        </Link>
+      </Button>
     </main>
-  )
+  );
 }
